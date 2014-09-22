99472df4965a69e8164a810af03ee07676faf9c8
Ryan Johnson <rhino.citguy@gmail.com>
2014-09-21 21:51:42 -0500

    auto restart after publish

--- config/deploy.rb
+++ config/deploy.rb
@@ -104,26 +104,6 @@ namespace :deploy do
     end
   end#:ensure_local_app_config
   before "deploy:check:linked_files", :ensure_local_app_config
+
+  after :publishing, "unicorn:restart"
 end#:deploy
-
-#namespace :deploy do
-#  desc 'Restart application'
-#  task :restart do
-#    on roles(:app), in: :sequence, wait: 5 do
-#      # Your restart mechanism here, for example:
-#      # execute :touch, release_path.join('tmp/restart.txt')
-#    end
-#  end
-#
-#  after :publishing, :restart
-#
-#  after :restart, :clear_cache do
-#    on roles(:web), in: :groups, limit: 3, wait: 10 do
-#      # Here we can do anything such as:
-#      # within release_path do
-#      #   execute :rake, 'cache:clear'
-#      # end
-#    end
-#  end
-#end
-
