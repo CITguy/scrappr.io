e86fc922a7faa972f07498f46039c4483bc4b9dc
Ryan Johnson <rhino.citguy@gmail.com>
2015-01-01 20:45:45 -0600

    add fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,10 @@
-Rails.application.config.assets.precompile += %w( select2.png select2-spinner.gif select2x2.png )
+Rails.application.config.assets.precompile += %w(
+  select2.png
+  select2-spinner.gif
+  select2x2.png
+  *.woff
+  *.ttf
+  *.eot
+  *.otf
+  *.svg
+)
