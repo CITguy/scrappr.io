681ec441e46edeefc65d30a8fc118d89075e9481
Ryan Johnson <rhino.citguy@gmail.com>
2015-01-01 19:58:40 -0600

    fix(config) production configuration tweaks
    * switch to using font-awesome-rails gem
    * tweak deploy scripts
    * tweak unicorn configs
    * tweaks to deploy paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -19,21 +21,44 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+
+### STAGING ###
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = false
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false #(prod:false)
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = false
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = false
+
+### END:STAGING ###
+
+
+### PRODUCTION ###
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  ## Compress JavaScripts and CSS.
+  #config.assets.js_compressor = :uglifier
+  ## config.assets.css_compressor = :sass
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
-  # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.2'
+  ## Version of your assets, change this if you want to expire all your assets.
+  #config.assets.version = '1.2'
+
+### END:PRODUCTION ###
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -63,7 +88,8 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # (2015-01-01: don't care at present)
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
