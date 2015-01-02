c712330675a99d40060b9385637ad29f4799d5b1
Ryan Johnson <rhino.citguy@gmail.com>
2015-01-01 20:30:43 -0600

    squash me

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,8 +52,8 @@ Rails.application.configure do
   #config.assets.js_compressor = :uglifier
   ## config.assets.css_compressor = :sass
 
-  ## Generate digests for assets URLs.
-  #config.assets.digest = true
+  # Generate digests for assets URLs.
+  config.assets.digest = true
 
   ## Version of your assets, change this if you want to expire all your assets.
   #config.assets.version = '1.2'
